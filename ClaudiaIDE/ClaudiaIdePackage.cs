0731a5e550144a013297bb0828e77f9dd9842508
buchizo <buchizo@live.jp>
2019-05-17 03:14:56 +0900

    update png #46

--- ClaudiaIDE/ClaudiaIdePackage.cs
+++ ClaudiaIDE/ClaudiaIdePackage.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 using ClaudiaIDE.Options;
 using Microsoft.VisualStudio.Shell;
 using System.Windows;
@@ -16,7 +16,7 @@ using Task = System.Threading.Tasks.Task;
 namespace ClaudiaIDE
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
-    [InstalledProductRegistration("#110", "#112", "2.1.0", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "2.1.0.1", IconResourceID = 400)]
     [ProvideOptionPage(typeof(ClaudiaIdeOptionPageGrid), "ClaudiaIDE", "General", 110, 116, true)]
     [Guid("7442ac19-889b-4699-a817-e6e054877ee3")]
     [ProvideAutoLoad(UIContextGuids.EmptySolution, PackageAutoLoadFlags.BackgroundLoad)]
